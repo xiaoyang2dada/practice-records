8b86b142d3b54bab0e112db1d2c61322349d67b7
xiaoyang2dada <xy2nahida@outlook.com>
2026-08-09 16:00:27 +0800

    删除了无用文件

--- August_task/src/models/YOLOv8/ultralytics/demo_predict.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-yolo = YOLO("./yolov8n.pt", task="detect")
-
-result = yolo(source="./ultralytics/assets/bus.jpg", save=True, conf=0.05)
--- August_task/src/models/YOLOv8/ultralytics/demo_train.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")
-
-model.train(data="yolo-bvn.yaml", worker=0, epochs=50, batch=16)
